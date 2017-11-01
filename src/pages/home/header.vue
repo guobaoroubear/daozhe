838db936d32c51472ea9b70c405fe59b2c559e43
17610350152 <17610350152@163.com>
2017-11-01 19:44:34 +0800

    bear

---
+++ src/pages/home/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="header">
+    <span><a><i></i></a></span>
+    <div>
+    	<input/>
+    </div>
+    <span>北京</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header'
+}
+</script>
+
+<style>
+#header {
+	background : #00bcd4;
+	width:100%;
+	height:.88rem
+}
+</style>
