65f70ed49c092452a73f761ee59dff5a2972522a
sumeixue <1366990556@qq.com>
2017-11-06 16:44:34 +0800

    susu

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="home">
-   		<Home-swipper></Home-swipper>
+   		<Home-swipper :bannerSwiperInfo="bannerSwiperInfo" :iconSwiperInfo="iconSwiperInfo"></Home-swipper>
    		<Home-suggest></Home-suggest>
    		<Home-week></Home-week>
 	</div>
@@ -10,13 +10,41 @@
 import HomeSwipper from './homeSwipper.vue';
 import HomeSuggest from './homeSuggest.vue';
 import HomeWeek from './homeWeek.vue';
+import axios from 'axios';
 export default {
   name: 'home',
   components: {
   	'Home-swipper': HomeSwipper,
   	'Home-suggest': HomeSuggest,
   	'Home-week': HomeWeek
+  },
+  data(){
+    return {
+      bannerSwiperInfo : [] ,
+      iconSwiperInfo : []
+    }
+  },
+  mounted (){
+    this.getHomeData()
+  },
+  methods: {
+    getHomeData (){
+      axios.get('/static/home.json?city=北京')
+      .then(this.handleGetDataSucc.bind(this))
+      .catch(this.handleGetDataErr.bind(this))
+    },
+    handleGetDataSucc (res){
+      console.log(res)
+      const data = res.data.data;
+      this.bannerSwiperInfo = data.bannerSwiperInfo
+      this.iconSwiperInfo = data.iconSwiperInfo
+    },
+    handleGetDataErr (){
+      alert("获取路径失败")
+    }
   }
+
+
 }
 </script>
 
